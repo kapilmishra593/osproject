6176a0c405001283542ada26a83abd47b5e8b7c5
kapilmishra593 <49762463+kapilmishra593@users.noreply.github.com>
2019-04-19 10:47:33 +0530

    Add files via upload

---
+++ osproject1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char p[10][5],temp[5];
+  int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
+  float avgwt;
+ printf("enter no of processes:");
+ scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  printf("enter process%d name:",i+1);
+ scanf("%s",&p[i]);
+  printf("enter process time:");
+ scanf("%d",&pt[i]);
+  printf("enter priority:");
+  scanf("%d",&pr[i]);
